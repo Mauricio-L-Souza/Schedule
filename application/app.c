8d19a7baa58fbbcdefebf3ea8d167135e76ca700
Mauricio-L-Souza <newmailmlcs@gmail.com>
2019-11-11 09:16:22 -0300

    remove debug log

--- application/app.c
+++ application/app.c
@@ -204,8 +204,6 @@ void birthdayList(FIELDS _contacts[], int index)
 //ARQUIVOS
 void close(FIELDS _records[], int index)
 {
-    printf("%d\n",index);
-    wait("");
     FILE *_file = fopen("_contacts.txtx", "wb");
 
     if(!_file) {
